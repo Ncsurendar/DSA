0cc60ad814d892c45c06a42f4095fbdab98fbdfb
Ncsurendar <ncsurendar123@gmail.com>
2024-05-15 18:10:10 +0530

    Initial Commit

---
+++ src/Arrays/SmallestNumber.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        int arr [] = {30,40,20,50,10};
+        int min = arr[4];
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
+
