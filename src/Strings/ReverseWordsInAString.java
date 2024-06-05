a0941412afadec818b364c66a6b990c9d15359ea
Ncsurendar <ncsurendar123@gmail.com>
2024-06-05 13:32:59 +0530

    String Builder and Split with spaces

---
+++ src/Strings/ReverseWordsInAString.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class ReverseWordsInAString {
+
+    public static String reverseWords(String s)
+    {
+        String [] words = s.split(" +");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=words.length-1; i>=0; i--)
+        {
+            sb.append(words[i]);
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        String ans = reverseWords(s);
+        System.out.println(ans);
+    }
+}
