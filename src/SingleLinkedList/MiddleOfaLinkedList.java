f90a87e505cebc80b72da571cb7d76010350df24
Ncsurendar <ncsurendar123@gmail.com>
2024-06-11 19:43:55 +0530

    Tortoise hare Algorithm

---
+++ src/SingleLinkedList/MiddleOfaLinkedList.java
@@ -0,0 +1,29 @@
+package SingleLinkedList;
+
+public class MiddleOfaLinkedList {
+
+    static Node middleNode(Node head)
+    {
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        head = middleNode(head);
+        System.out.println(head.data);
+
+    }
+}
