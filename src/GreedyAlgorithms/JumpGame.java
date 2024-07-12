4046650c4d249de4d149116e6ec9453649b1b96f
Ncsurendar <ncsurendar123@gmail.com>
2024-07-12 21:12:50 +0530

    Using Max Index method where that index holds the value of max jump and iterate through array

---
+++ src/GreedyAlgorithms/JumpGame.java
@@ -0,0 +1,33 @@
+package GreedyAlgorithms;
+
+public class JumpGame {
+
+    public static boolean canJump(int[] nums)
+    {
+        int maxIndex = 0;
+        for(int i=0; i<nums.length; i++)
+        {
+            if(i > maxIndex) return false;
+            maxIndex = Math.max(maxIndex, i + nums[i]);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 7, 1, 2};
+
+        System.out.print("Array representing maximum jump from each index: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        boolean ans = canJump(nums);
+
+        if (ans) {
+            System.out.println("It is possible to reach the last index.");
+        } else {
+            System.out.println("It is not possible to reach the last index.");
+        }
+    }
+}
