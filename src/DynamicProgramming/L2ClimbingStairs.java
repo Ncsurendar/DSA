0a3ca8e9d1f1549a215fae3825fe0c4091228e07
Ncsurendar <ncsurendar123@gmail.com>
2024-08-01 15:22:52 +0530

    Climbing Stairs Using Tabulation Method

---
+++ src/DynamicProgramming/L2ClimbingStairs.java
@@ -0,0 +1,24 @@
+package DynamicProgramming;
+
+public class L2ClimbingStairs {
+
+    static int climbStairs(int n)
+    {
+        if(n == 0 || n == 1) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++)
+        {
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("Number of ways to climb " + n + " stairs: " + climbStairs(n));
+    }
+}
