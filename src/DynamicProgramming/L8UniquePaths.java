e27dbd92844ca6dc2d236275a15ed367ca02ba52
Ncsurendar <ncsurendar123@gmail.com>
2024-08-04 19:53:37 +0530

    Using 2D Grid Method With Tabulation method

---
+++ src/DynamicProgramming/L8UniquePaths.java
@@ -0,0 +1,32 @@
+package DynamicProgramming;
+
+public class L8UniquePaths {
+
+    static int countWays(int m, int n)
+    {
+        int[][] dp = new int[m][n];
+
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i == 0 && j == 0) dp[i][j] = 1;
+                else
+                {
+                    int up = 0, left = 0;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+
+    public static void main(String[] args) {
+        int m = 3;
+        int n = 7;
+        System.out.println(countWays(m, n));
+    }
+}
